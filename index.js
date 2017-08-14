8ac6d80d0cab3b13e52a4becf471be8aa37f7319
sankett <sanket.terdal@gmail.com>
2017-08-14 12:45:03 +0530

    ch1

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/webhook1/', function (req, res) {
 app.post('/webhook/', function (req, res) {
 	console.log("request");
 	var data = req.body;
-	response = "This is a sample response from your webhook! " //Default response from the webhook to show it's working
+	response = req.body.result && req.body.result.parameters && req.body.result.parameters.source ? req.body.result.parameters.source :  "This is a sample response from your webhook! " //Default response from the webhook to show it's working
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(JSON.stringify({ "speech": response, "displayText": response 
