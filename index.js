d0801e8198ae4d81d5426a72707efda838b91105
sankett <sanket.terdal@gmail.com>
2017-08-14 14:31:51 +0530

    chg

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
+var bodyParser = require('body-parser');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.use(bodyParser.json())
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
