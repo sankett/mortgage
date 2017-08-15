e0ef988a8d78361cc13ab5e2ec54ff298d4cdb6c
sankett <sanket.terdal@gmail.com>
2017-08-15 19:11:11 +0530

    change

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/webhook/', function (req, res) {
 	   var repay = result.parameters["mort-repay"];
 	   var rate = result.parameters["mort-rate"];
 	   var name = result.parameters["mort-name"];
-	   result =  "Type:" + morttype + " | Buyer:" + buyer + "| deposit: $" + deposit + " | Earn: $" + earn;
+	   result =  "|----Thank You. Here are the details-----| Type:" + morttype + " | Buyer:" + buyer + "| deposit: $" + deposit + " | Earn: $" + earn;
 	   result += " Credit Card balances: $" + cc + " | Monthly repayments: $" + repay;
 	   result += " Rate: " + rate + " | Name:" + name ;
 	
