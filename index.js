956f3cf59b7548922c7c73b181f175ab4f39056a
sankett <sanket.terdal@gmail.com>
2017-08-15 19:25:19 +0530

    chg

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ app.post('/webhook/', function (req, res) {
 	   var rate = result.parameters["mort-rate"];
 	   var name = result.parameters["mort-name"];
 	   var num = uuid.v4(); 
-	   result =  "|----Thank You. Here are the details-----|" + num;
+	   result =  "|----Thank You.Please note details-----|" ;
+	   result += "Reference no:" + num;
+	   result += "======Details=====";
 	   result += " Type:" + morttype + " | Buyer:" + buyer + "| deposit: $" + deposit + " | Earn: $" + earn;
 	   result += " Credit Card balances: $" + cc + " | Monthly repayments: $" + repay;
 	   result += " Rate: " + rate + " | Name:" + name ;
